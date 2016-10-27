c795d6679a89166f814a38bbeae68dc3cf44253c
Nick <nick.benoit14@gmail.com>
2016-10-27 08:31:51 -0600

    add upload_course

--- app/services/scorm_cloud_service.rb
+++ app/services/scorm_cloud_service.rb
@@ -12,22 +12,27 @@ class ScormCloudService
     end
   end
 
-  def upload_course
-    scorm_cloud_request do
-      course = ScormCourse.create
-      response = scorm_cloud.course.import_course(course.id, params[:filename])
+  def upload_course(file)
+    course = ScormCourse.create
+    cleanup = Proc.new { course.destroy }
+    scorm_cloud_request(cleanup) do
+      response = @scorm_cloud.course.import_course(course.id, file)
     end
   end
 
-	def scorm_cloud_request
+	def scorm_cloud_request(handle_fail = nil)
 		begin
-			return yield
+      response = {status: 200}
+      response[:response] = yield
+			return response
 		rescue ScormCloud::InvalidPackageError => e
-		  return {error: e.to_json()}
+		  response = {error: e.to_s, status: 400}
 		rescue ScormCloud::RequestError => e
-			return {error: e.to_json()}
+			response = {error: e.to_s, status:400}
 		rescue ScormCloud::Error => e
-			return {error: e.to_json}
+			response = {error: e.to_s, status: 400}
 		end
+    handle_fail.call if handle_fail.respond_to? :call
+    return response
 	end
 end
