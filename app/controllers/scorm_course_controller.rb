85bb724825fea8f0f6bd59579c0bb921a812d872
Nick <nick.benoit14@gmail.com>
2016-10-27 14:10:59 -0600

    handle launch error

--- app/controllers/scorm_course_controller.rb
+++ app/controllers/scorm_course_controller.rb
@@ -8,13 +8,17 @@ class ScormCourseController < ApplicationController
   def create
 		launch = @scorm_cloud.launch_course(
       scorm_course_id: params[:course_id],
-			lms_user_id: params[:custom_canvas_user_id], #TODO use canvas user id to create registrations
-			first_name: params[:lis_person_name_given],  # TODO use given, and family name from params
-      last_name: params[:lis_person_name_family],  # TODO use given, and family name from params
+			lms_user_id: params[:custom_canvas_user_id], 
+			first_name: params[:lis_person_name_given],
+      last_name: params[:lis_person_name_family],
 			redirect_url: lti_launches_url # TODO create finished with test endpoint
 		)
 
-    redirect_to launch[:response]
+    if launch[:status] == 200
+      redirect_to launch[:response]
+    else
+      format.html { render file: "public/401.html", status: :unauthorized }
+    end
   end
 
   private
