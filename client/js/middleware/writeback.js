725ddb9a06d6ac07f3a060c420716d5b1cf6be4c
Nick <nick.benoit14@gmail.com>
2016-11-03 16:57:34 -0600

    add writeback middleware

--- client/js/middleware/writeback.js
+++ client/js/middleware/writeback.js
@@ -1,10 +1,15 @@
+import * as PackageActions from "../actions/scorm";
 import { Constants as PackageConstants }   from "../actions/scorm";
 import { DONE }    from "../constants/wrapper";
 
 const Writeback = store => next => action => {
   switch(action.type){
     case "CREATE_ASSIGNMENT_DONE":
-      
+      const state = store.getState();
+      store.dispatch(PackageActions.updatePackage(
+        action.payload.integration_id,
+        {lms_assignment_id: action.payload.id}
+      ));
       break;
   }
 
