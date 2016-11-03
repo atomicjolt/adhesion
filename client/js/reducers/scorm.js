a4841826b75a03f326e812feb97375409522252a
Cory-Christensen <christensen.corym@gmail.com>
2016-11-03 11:56:45 -0600

    removing unnecessary deletes

--- client/js/reducers/scorm.js
+++ client/js/reducers/scorm.js
@@ -15,7 +15,7 @@ export default (state = initialState, action) => {
         scormList: action.payload.response,
         shouldRefreshList: false,
         file: null
-      }; //TODO check if always is an array
+      }; 
 
     case PackageConstants.UPLOAD_PACKAGE:
       let file = action.upload;
@@ -31,12 +31,9 @@ export default (state = initialState, action) => {
       }
 
     case PackageConstants.UPDATE_UPLOAD_FILE:
-      // TODO: need to update state with response (this is for error)
       return {...state, file: action.file};
 
     case PackageConstants.REMOVE_ERROR:
-      delete state.file;
-      delete state.uploadError;
       return {...state, file: null, uploadError: false};
 
     default:
