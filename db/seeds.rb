04afd4b9ffeebb7a2519d844023a44641cb0ec1a
Nick <nick.benoit14@gmail.com>
2016-11-05 08:33:16 -0600

    give scorm app delete assignment permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ lti_applications = [
   lti_consumer_uri: "https://atomicjolt.instructure.com",
   lti_secret: Rails.application.secrets.scorm_lti_key,
   canvas_token: Rails.application.secrets.canvas_token,
-  canvas_api_permissions: "CREATE_ASSIGNMENT" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
+  canvas_api_permissions: "CREATE_ASSIGNMENT,DELETE_ASSIGNMENT" # List comma separated Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
 },
 {
   name: "Attendance",
@@ -20,7 +20,7 @@ lti_applications = [
   client_application_name: "attendance",
   lti_consumer_uri: "https://atomicjolt.instructure.com",
   lti_secret: Rails.application.secrets.attendance_lti_key,
-  canvas_api_permissions: "" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
+  canvas_api_permissions: "" # List of comma separated Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
 }]
 
 lti_applications.each do |attrs|
