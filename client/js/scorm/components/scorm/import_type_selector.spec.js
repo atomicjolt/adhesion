4fe24b86b8ca999bf1e85f496e3604d5eaa0c7ce
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-11-14 14:38:45 -0700

    completed import_type_selector spec

---
+++ client/js/scorm/components/scorm/import_type_selector.spec.js
@@ -0,0 +1,34 @@
+/* global describe beforeEach it expect */
+
+import React      from 'react';
+import TestUtils  from 'react/lib/ReactTestUtils';
+import ImportTypeSelector from './import_type_selector';
+import Stub      from '../../../../specs_support/stub.jsx';
+
+describe('import type selector', () => {
+  let props, result;
+  beforeEach(() => {
+    props = {handleSelectChange: (e) => {props.handleSelectChange = e.target.value},
+             handleGoClick: () => {props.isGoBtnActive = true},
+             isGoBtnActive: false
+            }
+    result = TestUtils.renderIntoDocument(<Stub><ImportTypeSelector {...props}/></Stub>);
+  });
+
+  it('Go button calls callback on click', () => {
+    expect(props.isGoBtnActive).toBeFalsy();
+    const button = TestUtils.findRenderedDOMComponentWithClass(result, 'c-btn--go');
+    TestUtils.Simulate.click(button);
+    expect(props.isGoBtnActive).toBeTruthy();
+  });
+
+  it('select change activated on change', () => {
+    const selection = TestUtils.findRenderedDOMComponentWithTag(result, 'select');
+    TestUtils.Simulate.change(selection, {target: {value: 'Graded Assignment'}});
+    expect(props.handleSelectChange).toEqual('Graded Assignment');
+    TestUtils.Simulate.change(selection, {target: {value: 'Ungraded Assignment'}});
+    expect(props.handleSelectChange).toEqual('Ungraded Assignment');
+    TestUtils.Simulate.change(selection, {target: {value: 0}});
+    expect(props.handleSelectChange).toEqual(0);
+  });
+});
