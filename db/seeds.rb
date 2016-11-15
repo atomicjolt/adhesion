d1d1d145552f7aa778952a0e02d72db3d54328cb
Justin Ball <justinball@gmail.com>
2016-11-14 19:31:17 -0700

    Fixes default application seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,23 @@ lti_applications = [{
   canvas_api_permissions: "LIST_USERS_IN_COURSE_USERS" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
 }]
 
+if Rails.env.dev?
+  lti_consumer_uri = "https://atomicjolt.instructure.com"
+else
+  lti_consumer_uri = "https://au.openlmshost.com"
+end
+
 lti_application_instances = [{
   lti_application: "SCORM Player",
   lti_key: "scorm-player",
   lti_secret: Rails.application.secrets.scorm_lti_key,
-  lti_consumer_uri: "https://au.openlmshost.com",
+  lti_consumer_uri: lti_consumer_uri,
   canvas_token: Rails.application.secrets.canvas_token # This is only required if the app needs API access and doesn't want each user to do the oauth dance
 },{
   lti_application: "Attendance",
   lti_key: "attendance",
   lti_secret: Rails.application.secrets.attendance_lti_key,
-  lti_consumer_uri: "https://au.openlmshost.com",
+  lti_consumer_uri: lti_consumer_uri,
   canvas_token: Rails.application.secrets.canvas_token # This is only required if the app needs API access and doesn't want each user to do the oauth dance
 }]
 
