d3867e1a46fcec882fd08bd5197dd89440c7d4f3
Cory-Christensen <christensen.corym@gmail.com>
2016-11-18 15:09:06 -0700

    Adds progress loader instead of progress bar

--- client/js/scorm/components/scorm/uploader.jsx
+++ client/js/scorm/components/scorm/uploader.jsx
@@ -6,39 +6,47 @@ import CoursesList              from './courses_list';
 import * as ScormActions        from '../../actions/scorm';
 import CommonSvg                from '../common/common_svg';
 import SvgButton                from '../common/svg_button';
+import Loader                   from '../../../common_components/loader';
 
 export class Uploader extends React.Component {
 
-  renderProgress() {
-    if (this.props.error) {
-      return (
-        <div>
-          <div className="c-upload-error">
-            <CommonSvg className="c-icon-error" type="error" />
-            <span>This file is not a valid SCORM package.</span>
-          </div>
-          <div className="c-progress-bar red">
-            <span style={{width: "100%"}}></span>
-          </div>
-          <SvgButton handleClick={(e)=>this.props.removeError()} type="removeError" />
-        </div>
-      );
-    } else {
-      return (
-        <div className="c-progress-bar">
-          <span style={{width: "100%"}}><span className="progress blue"></span></span>
-        </div>
-      );
+  getStyles(){
+    return {
+      loaderContainer: {
+        position: 'absolute',
+        right: '50px',
+        top: '0px'
+      }
     }
   }
 
+  renderError() {
+    return (
+      <div>
+        <span className="c-upload-error">
+          <CommonSvg className="c-icon-error" type="error" />
+          <span>This file is not a valid SCORM package.</span>
+        </span>
+        <SvgButton handleClick={(e)=>this.props.removeError()} type="removeError" />
+      </div>
+    );
+  }
+
   render() {
+
+    let renderProgress;
+    if (this.props.error) {
+      renderProgress = this.renderError();
+    } else {
+      renderProgress = <div style={this.getStyles().loaderContainer}><Loader/></div>;
+    }
+
     return (
       <ul className="c-upload">
         <li className="c-list__upload">
           <CommonSvg className="c-icon-upload" type="upload"/>
           <div className="c-list-item__title">{this.props.scormFile.name}</div>
-          {this.renderProgress()}
+          {renderProgress}
         </li>
       </ul>
     );
