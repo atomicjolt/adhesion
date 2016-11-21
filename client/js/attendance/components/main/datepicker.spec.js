f103cfbda5b6a30088514a2785c118a98aa36246
bensmith <benhsmith13@gmail.com>
2016-11-21 16:53:07 -0700

    check this spec

---
+++ client/js/attendance/components/main/datepicker.spec.js
@@ -0,0 +1,15 @@
+import React        from 'react';
+import TestUtils    from 'react/lib/ReactTestUtils';
+import DatePicker   from './datepicker.jsx';
+
+describe('date picker', () => {
+  let result;
+
+  beforeEach(() => {
+    result = TestUtils.renderIntoDocument(<DatePicker/>);
+  });
+
+  it('renders...', () => {
+    expect(TestUtils.findRenderedDOMComponentWithClass(result, 'c-date-calendar')).not.toBeDefined();
+  });
+});
