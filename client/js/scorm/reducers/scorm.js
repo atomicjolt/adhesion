42ec56993bff54b2da5fb3fd648617ba602ae7bd
dittonjs <jditton.atomic@gmail.com>
2016-12-01 16:12:09 -0700

    keep canvas assignments and scorm assignment in sync [#134403191] (#29)
    * keep canvas assignments and scorm assignment in sync [#134403191]
    
    * fix hound
    
    * fix issues from pull review

--- client/js/scorm/reducers/scorm.js
+++ client/js/scorm/reducers/scorm.js
@@ -1,63 +1,70 @@
-import { Constants as PackageConstants }   from "../actions/scorm";
+import _ from 'lodash';
+import { Constants as PackageConstants } from '../actions/scorm';
 
 const initialState = {
-  scormList: [],
-  shouldRefreshList: false
+  scormList: null,
+  shouldRefreshList: false,
+  canvasAssignments: null,
 };
 
 export default (state = initialState, action) => {
-  switch(action.type){
+  switch (action.type) {
 
-    case "CREATE_ASSIGNMENT":
-      var updatedScorm = {...state.scormList[action.localData.index]};
+    case 'CREATE_ASSIGNMENT': {
+      const updatedScorm = { ...state.scormList[action.localData.index] };
       updatedScorm.fetching = true;
-      var updatedScormList = state.scormList.slice();
+      const updatedScormList = state.scormList.slice();
       updatedScormList[action.localData.index] = updatedScorm;
-      return {...state, scormList: updatedScormList};
-
-    case "CREATE_ASSIGNMENT_DONE":
-      var updatedScorm = {...state.scormList[action.original.localData.index]};
+      return { ...state, scormList: updatedScormList };
+    }
+    case 'CREATE_ASSIGNMENT_DONE': {
+      const updatedScorm = { ...state.scormList[action.original.localData.index] };
       updatedScorm.fetching = false;
-      var updatedScormList = state.scormList.slice();
+      const updatedScormList = state.scormList.slice();
       updatedScormList[action.original.localData.index] = updatedScorm;
-      return {...state, scormList: updatedScormList};
+      return { ...state, scormList: updatedScormList };
+    }
+    case 'LIST_ASSIGNMENTS_DONE':
+      return { ...state, canvasAssignments: action.payload };
 
-    case PackageConstants.UPDATE_IMPORT_TYPE:
-      var updatedScorm = {...state.scormList[action.index]};
+    case PackageConstants.UPDATE_IMPORT_TYPE: {
+      const updatedScorm = { ...state.scormList[action.index] };
       updatedScorm.is_graded = action.importType;
-      var updatedScormList = state.scormList.slice();
+      const updatedScormList = state.scormList.slice();
       updatedScormList[action.index] = updatedScorm;
-      return {...state, scormList: updatedScormList};
-
+      return { ...state, scormList: updatedScormList };
+    }
     case PackageConstants.LOAD_PACKAGES_DONE:
       return {
         ...state,
-        scormList: action.payload.response.map((item, index) => {
-          item.index = index;
-          return item;
-        }),
+        scormList: action.payload.response.map((item, index) => ({ ...item, index })),
         shouldRefreshList: false,
-        file: null
+        file: null,
       };
 
-    case PackageConstants.UPLOAD_PACKAGE:
-      let file = action.upload;
-      let showUpload = true;
-      return {...state, showUploading: showUpload, file};
+    case PackageConstants.UPLOAD_PACKAGE: {
+      const file = action.upload;
+      const showUpload = true;
+      return { ...state, showUploading: showUpload, file };
+    }
+    case PackageConstants.REMOVE_PACKAGE: {
+      const newState = _.cloneDeep(state);
+      _.remove(newState.scormList, scorm => scorm.id === action.courseId);
+      return newState;
+    }
 
     case PackageConstants.REMOVE_PACKAGE_DONE:
-    case PackageConstants.UPLOAD_PACKAGE_DONE:
+    case PackageConstants.UPLOAD_PACKAGE_DONE: {
       if (action.error) {
-        return {...state, file: action.original.upload, uploadError: true};
-      } else {
-        return {...state, file: null, shouldRefreshList: true};
+        return { ...state, file: action.original.upload, uploadError: true };
       }
-
+      return { ...state, file: null, shouldRefreshList: true };
+    }
     case PackageConstants.UPDATE_UPLOAD_FILE:
-      return {...state, file: action.file};
+      return { ...state, file: action.file };
 
     case PackageConstants.REMOVE_ERROR:
-      return {...state, file: null, uploadError: false};
+      return { ...state, file: null, uploadError: false };
 
     default:
       return state;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 admin = CreateAdminService.create_admin
-puts 'CREATED ADMIN USER: ' << admin.email
+puts "CREATED ADMIN USER: " << admin.email
 
 # Add an LTI Application
+scorm_permissions = "CREATE_ASSIGNMENT,DELETE_ASSIGNMENT,LIST_ASSIGNMENTS"
 lti_applications = [{
   name: "SCORM Player",
   description: "SCORM Player",
   client_application_name: "scorm",
-  canvas_api_permissions: "CREATE_ASSIGNMENT,DELETE_ASSIGNMENT" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
-},{
+  canvas_api_permissions: scorm_permissions,
+}, {
   name: "Attendance",
   description: "Attendance Application",
   client_application_name: "attendance",
-  canvas_api_permissions: "LIST_USERS_IN_COURSE_USERS" # List Canvas API methods the app is allowed to use. A full list of constants can be found in canvas_urls
+  canvas_api_permissions: "LIST_USERS_IN_COURSE_USERS",
 }]
 
 lti_consumer_uri = Rails.application.secrets.canvas_url
@@ -21,13 +22,13 @@ lti_application_instances = [{
   lti_key: "scorm-player",
   lti_secret: Rails.application.secrets.scorm_lti_secret,
   lti_consumer_uri: lti_consumer_uri,
-  canvas_token: Rails.application.secrets.canvas_token # This is only required if the app needs API access and doesn't want each user to do the oauth dance
-},{
+  canvas_token: Rails.application.secrets.canvas_token,
+}, {
   lti_application: "Attendance",
   lti_key: "attendance",
   lti_secret: Rails.application.secrets.attendance_lti_secret,
   lti_consumer_uri: lti_consumer_uri,
-  canvas_token: Rails.application.secrets.canvas_token # This is only required if the app needs API access and doesn't want each user to do the oauth dance
+  canvas_token: Rails.application.secrets.canvas_token,
 }]
 
 lti_applications.each do |attrs|
@@ -41,12 +42,13 @@ end
 lti_application_instances.each do |attrs|
   lti_application = LtiApplication.find_by(name: attrs.delete(:lti_application))
   attrs = attrs.merge(lti_application_id: lti_application.id)
-  if lti_application_instance = LtiApplicationInstance.find_by(lti_key: attrs[:lti_key])
+  if lti_application_instance = LtiApplicationInstance.find_by(
+    lti_key: attrs[:lti_key],
+  )
     lti_application_instance.update_attributes!(attrs)
   else
     LtiApplicationInstance.create!(attrs)
   end
 end
-
 
 Lti::Utils.lti_configs
