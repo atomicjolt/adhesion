2cf5de55ff991fbdfb3681e01d13b7aa7880268c
dittonjs <jditton.atomic@gmail.com>
2016-12-02 14:10:21 -0700

    starting auth stuff

--- app/controllers/scorm_course_controller.rb
+++ app/controllers/scorm_course_controller.rb
@@ -13,7 +13,8 @@ class ScormCourseController < ApplicationController
       first_name: params[:lis_person_name_given],
       last_name: params[:lis_person_name_family],
       redirect_url: params[:launch_presentation_return_url],
-      postback_url: scorm_course_postback_url,
+      # postback_url: scorm_course_postback_url,
+      postback_url: "https://c6e6e17a.ngrok.io/scorm_course/postback",
       lti_credentials: current_lti_application_instance,
       result_params: params
     )
