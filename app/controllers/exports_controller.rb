ef3f18465b871d17e3875368fa061f6af2d75754
dittonjs <jditton.atomic@gmail.com>
2016-12-06 10:19:28 -0700

    makes it so that students allways show up in csv

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,11 +1,9 @@
 class ExportsController < ApplicationController
   include Concerns::JwtToken
-
+  include Concerns::CanvasSupport
   before_action :validate_token
 
   def attendances
-
-
     respond_to do |format|
       format.json do
         render json: get_attendances
@@ -13,9 +11,17 @@ class ExportsController < ApplicationController
 
       format.csv do
         @attendances = get_attendances
-        
-        headers['Content-Type'] = "text/csv"
-        headers['Content-Disposition'] = "attachment; filename=\"attendance.csv\""
+        @students = canvas_api.proxy(
+          "LIST_USERS_IN_COURSE_USERS",
+          {
+            course_id: params[:course_id],
+            enrollment_type: ["student"],
+          },
+          request.body.read,
+        ).parsed_response
+        content_disposition = "attachment; filename=\"attendance.csv\""
+        headers["Content-Type"] = "text/csv"
+        headers["Content-Disposition"] = content_disposition
       end
     end
   end
@@ -24,7 +30,7 @@ class ExportsController < ApplicationController
 
   def get_attendances
     attendances = Attendance.where(lms_course_id: params[:course_id])
-    if(params[:startDate] && params[:endDate])
+    if params[:startDate] && params[:endDate]
       attendances = attendances.
         where("date <= ?", Date.parse(params[:endDate])).
         where("date >= ?", Date.parse(params[:startDate]))
