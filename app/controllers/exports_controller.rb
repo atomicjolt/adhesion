601f968649c482d8781669f14f3a333c46e5ed64
dittonjs <jditton.atomic@gmail.com>
2016-12-06 14:40:21 -0700

    move csv logic to controller

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -4,26 +4,45 @@ class ExportsController < ApplicationController
   before_action :validate_token
 
   def attendances
-    respond_to do |format|
-      format.json do
-        render json: get_attendances
-      end
+    @attendances = get_attendances
+    @students = canvas_api.proxy(
+      "LIST_USERS_IN_COURSE_USERS",
+      {
+        course_id: params[:course_id],
+        enrollment_type: ["student"],
+      },
+      request.body.read,
+    ).parsed_response
 
-      format.csv do
-        @attendances = get_attendances
-        @students = canvas_api.proxy(
-          "LIST_USERS_IN_COURSE_USERS",
-          {
-            course_id: params[:course_id],
-            enrollment_type: ["student"],
-          },
-          request.body.read,
-        ).parsed_response
-        content_disposition = "attachment; filename=\"attendance.csv\""
-        headers["Content-Type"] = "text/csv"
-        headers["Content-Disposition"] = content_disposition
+    final_csv = CSV.generate do |csv|
+      days = @attendances.group_by(&:date).keys.sort
+      header_row = ["Name"].concat days
+      csv << header_row
+      sorted_students = @students.sort_by { |s| s["sortable_name"] }
+      sorted_students.each do |student|
+        if student["name"].include?(",")
+          parts = student["name"].split(",")
+          parts = parts.map(&:strip)
+          last = parts.shift
+          row = [parts.join(" ") << " " << last]
+        else
+          row = [student["name"]]
+        end
+
+        days.each do |day|
+          att = @attendances.detect do |attendance|
+            attendance.date == day && student["id"] == attendance.lms_student_id
+          end
+          row << if att.present? && att.status.present?
+                   att.status.downcase
+                 else
+                   "n/a"
+                 end
+        end
+        csv << row
       end
     end
+    send_data(final_csv, filename: "attendance_export")
   end
 
   private
