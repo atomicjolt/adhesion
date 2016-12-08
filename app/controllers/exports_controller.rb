34e8f68a1502a602fced543a8a8f23728f9f56b2
dittonjs <jditton.atomic@gmail.com>
2016-12-07 17:33:57 -0700

    move to model

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -4,8 +4,8 @@ class ExportsController < ApplicationController
   before_action :validate_token
 
   def attendances
-    @attendances = get_attendances
-    @students = canvas_api.proxy(
+    attendances = get_attendances
+    students = canvas_api.proxy(
       "LIST_USERS_IN_COURSE_USERS",
       {
         course_id: params[:course_id],
@@ -14,34 +14,7 @@ class ExportsController < ApplicationController
       request.body.read,
     ).parsed_response
 
-    final_csv = CSV.generate do |csv|
-      days = @attendances.group_by(&:date).keys.sort
-      header_row = ["Name"].concat days
-      csv << header_row
-      sorted_students = @students.sort_by { |s| s["sortable_name"] }
-      sorted_students.each do |student|
-        if student["name"].include?(",")
-          parts = student["name"].split(",")
-          parts = parts.map(&:strip)
-          last = parts.shift
-          row = [parts.join(" ") << " " << last]
-        else
-          row = [student["name"]]
-        end
-
-        days.each do |day|
-          att = @attendances.detect do |attendance|
-            attendance.date == day && student["id"] == attendance.lms_student_id
-          end
-          row << if att.present? && att.status.present?
-                   att.status.downcase
-                 else
-                   "n/a"
-                 end
-        end
-        csv << row
-      end
-    end
+    final_csv = Attendance.generate_csv(students, attendances)
     send_data(final_csv, filename: "attendance_export")
   end
 
