47c927d4ca8ec4721ccb93055e33b6a95acb58f2
Justin Ball <justinball@gmail.com>
2016-12-14 21:30:23 -0700

    Styling

--- client/js/attendance/components/layout/index.jsx
+++ client/js/attendance/components/layout/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import React                    from "react";
-
 export default class Index extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        {this.props.children}
+        { this.props.children }
       </div>
     );
   }
