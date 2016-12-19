bc4660338db5e98e633c7632f4204644430bdc0b
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-12-19 14:43:28 -0700

    travis spec fix

--- spec/controllers/scorm_courses_controller_spec.rb
+++ spec/controllers/scorm_courses_controller_spec.rb
@@ -42,48 +42,91 @@ RSpec.describe ScormCoursesController, type: :controller do
   end
 
   describe "Create" do
-    before do
+    it "should handle the successful launch of a new SCORM course" do
+      allow_any_instance_of(ScormCloud::ScormCloud).to receive(
+        :registration,
+      ).and_return(MockScorm.new)
       @lti_application_instance = FactoryGirl.create(:lti_application_instance)
       allow(controller).to receive(
         :current_lti_application_instance,
       ).and_return(@lti_application_instance)
-      reg = Registration.create(lms_user_id: 2)
-      stub_request(:get, /cloud.scorm.com/).
-        to_return(status: 200,
-                  body: "<rsp>
-                         <registrationreport
-                           format='summary'
-                           regid='#{reg.id}'
-                           instanceid='0'>
-                           <complete>complete</complete>
-                           <success>failed</success>
-                           <totaltime>19</totaltime>
-                           <score>0</score>
-                         </registrationreport>
-                       </rsp>",
-                  headers: {})
+      Registration.create(lms_user_id: 2)
+      scu = scorm_courses_url
       @params = lti_params(@lti_application_instance.lti_key,
                            @lti_application_instance.lti_secret,
                            "custom_canvas_user_id" => 2,
                            "course_id" => 1,
                            "launch_url" => scorm_courses_url,
-                           "roles" => "Learner")
-    end
-    it "should handle the setup method" do
+                           "roles" => "Learner",
+                           "launch_presentation_return_url" => scu)
       request.env["CONTENT_TYPE"] = "application/x-www-form-urlencoded"
       post :create, @params
       expect(assigns(:scorm_cloud)).to_not eq(nil)
-    end
-
-    it "should handle the successful launch of a new SCORM course" do
-      request.env["CONTENT_TYPE"] = "application/x-www-form-urlencoded"
-      post :create, @params
       expect(response.status).to eq(302)
     end
 
-    it "should handle the failed creation of a new SCORM course" do
+    it "should handle the failed launch of a new SCORM course" do
+      allow_any_instance_of(ScormCloud::ScormCloud).to receive(
+        :registration,
+      ).and_return(MockScorm.new)
+      @lti_application_instance = FactoryGirl.create(:lti_application_instance)
+      allow(controller).to receive(
+        :current_lti_application_instance,
+      ).and_return(@lti_application_instance)
+      Registration.create(lms_user_id: 2)
+      scu = scorm_courses_url
+      @params = lti_params(@lti_application_instance.lti_key,
+                           @lti_application_instance.lti_secret,
+                           "custom_canvas_user_id" => 2,
+                           "course_id" => 1,
+                           "launch_url" => scorm_courses_url,
+                           "roles" => "Learner",
+                           "launch_presentation_return_url" => scu)
       post :create, @params
       expect(response.status).to eq(401)
     end
+  end
+end
+
+class MockScorm
+  @status = 200
+  def launch(registration_id, redirect_url)
+    response = "status: 200,
+      body: '<rsp>
+               <registrationreport
+                 format='summary'
+                 regid='#{registration_id}'
+                 instanceid='0'>
+                 <complete>complete</complete>
+                 <success>failed</success>
+                 <totaltime>19</totaltime>
+                 <score>0</score>
+               </registrationreport>
+             </rsp>',
+      headers: {},
+      #{redirect_url}"
+    response
+  end
+
+  def get_registration_result(registration_id)
+    response = "{
+      status: 200,
+      body: '<rsp>
+               <registrationreport
+                 format='summary'
+                 regid='#{registration_id}'
+                 instanceid='0'>
+                 <complete>complete</complete>
+                 <success>failed</success>
+                 <totaltime>19</totaltime>
+                 <score>0</score>
+               </registrationreport>
+             </rsp>',
+      headers: {},
+    }"
+    response
+  end
+
+  def create_registration
   end
 end
