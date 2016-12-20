3f6fc5666b0be8533778340f307ac4fc3c562fd2
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-12-20 07:55:53 -0700

    start of attendance server specs

---
+++ app/controllers/scorm_courses_controller.rb
@@ -0,0 +1,53 @@
+class ScormCoursesController < ApplicationController
+  include Concerns::LtiSupport
+
+  protect_from_forgery with: :null_session
+  before_action :setup
+  before_action :validate_postback_credentials, only: [:postback]
+  before_action :do_lti, only: [:create]
+
+  def create
+    launch = @scorm_cloud.launch_course(
+      scorm_course_id: params[:course_id],
+      lms_user_id: params[:custom_canvas_user_id],
+      first_name: params[:lis_person_name_given],
+      last_name: params[:lis_person_name_family],
+      redirect_url: params[:launch_presentation_return_url],
+      postback_url: scorm_courses_postback_url,
+      lti_credentials: current_lti_application_instance,
+      result_params: params,
+    )
+
+    @scorm_cloud.sync_registration(params)
+    if launch[:status] == 200
+      redirect_to launch[:response]
+    else
+      render file: "public/401.html", status: :unauthorized
+    end
+  end
+
+  def postback
+    response = Hash.from_xml(params[:data])
+    @scorm_cloud.sync_registration_score(response["registrationreport"])
+    render json: {}, status: 200
+  end
+
+  private
+
+  def validate_postback_credentials
+    response = Hash.from_xml(params[:data])
+    registration_report = response["registrationreport"]
+    reg_id = registration_report && registration_report["regid"]
+    begin
+      reg = Registration.find(reg_id)
+      raise ScormCloudError.new if reg.scorm_cloud_passback_secret !=
+          params[:password]
+    rescue ScormCloudError, ActiveRecord::RecordNotFound
+      render json: { error: "Not Authorized" }, status: 400
+    end
+  end
+
+  def setup
+    @scorm_cloud = ScormCloudService.new
+  end
+end
---
+++ spec/controllers/scorm_courses_controller_spec.rb
@@ -0,0 +1,117 @@
+require "rails_helper"
+
+RSpec.describe ScormCoursesController, type: :controller do
+  describe "POST#postback" do
+    it "should reject bad password" do
+      reg = Registration.create
+      post(
+        :postback,
+        data:
+          "<registrationreport
+            format='summary'
+            regid='#{reg.id}'
+            instanceid='0'>
+              <complete>complete</complete>
+              <success>failed</success>
+              <totaltime>19</totaltime>
+              <score>0</score>
+          </registrationreport>",
+        password: "FakePass",
+      )
+      expect(response.status).to equal(400)
+    end
+
+    it "should accept good password" do
+      reg = Registration.create
+      post(
+        :postback,
+        data:
+          "<registrationreport
+            format='summary'
+            regid='#{reg.id}'
+            instanceid='0'>
+              <complete>complete</complete>
+              <success>failed</success>
+              <totaltime>19</totaltime>
+              <score>0</score>
+          </registrationreport>",
+        password: reg.scorm_cloud_passback_secret,
+      )
+      expect(response.status).to equal(200)
+    end
+  end
+
+  describe "Create" do
+    before(:example) do
+      allow_any_instance_of(ScormCloud::ScormCloud).to receive(
+        :registration,
+      ).and_return(MockScorm.new)
+      @lti_application_instance = FactoryGirl.create(:lti_application_instance)
+      allow(controller).to receive(
+        :current_lti_application_instance,
+      ).and_return(@lti_application_instance)
+      Registration.create(lms_user_id: 2)
+      scu = scorm_courses_url
+      @params = lti_params(@lti_application_instance.lti_key,
+                           @lti_application_instance.lti_secret,
+                           "custom_canvas_user_id" => 2,
+                           "course_id" => 1,
+                           "launch_url" => scorm_courses_url,
+                           "roles" => "Learner",
+                           "launch_presentation_return_url" => scu)
+    end
+    it "should handle the successful launch of a new SCORM course" do
+      request.env["CONTENT_TYPE"] = "application/x-www-form-urlencoded"
+      post :create, @params
+      expect(assigns(:scorm_cloud)).to_not eq(nil)
+      expect(response.status).to eq(302)
+    end
+    it "should handle the failed launch of a new SCORM course" do
+      post :create, @params
+      expect(response.status).to eq(401)
+    end
+  end
+end
+
+class MockScorm
+  @status = 200
+  def launch(registration_id, redirect_url)
+    response = "status: 200,
+      body: '<rsp>
+               <registrationreport
+                 format='summary'
+                 regid='#{registration_id}'
+                 instanceid='0'>
+                 <complete>complete</complete>
+                 <success>failed</success>
+                 <totaltime>19</totaltime>
+                 <score>0</score>
+               </registrationreport>
+             </rsp>',
+      headers: {},
+      #{redirect_url}"
+    response
+  end
+
+  def get_registration_result(registration_id)
+    response = "{
+      status: 200,
+      body: '<rsp>
+               <registrationreport
+                 format='summary'
+                 regid='#{registration_id}'
+                 instanceid='0'>
+                 <complete>complete</complete>
+                 <success>failed</success>
+                 <totaltime>19</totaltime>
+                 <score>0</score>
+               </registrationreport>
+             </rsp>',
+      headers: {},
+    }"
+    response
+  end
+
+  def create_registration
+  end
+end
