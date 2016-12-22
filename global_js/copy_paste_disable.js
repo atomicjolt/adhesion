65936dac3f6f463411b3d57bdd4f708dc234dc8e
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-12-22 11:02:41 -0700

    more linter fixes

--- global_js/copy_paste_disable.js
+++ global_js/copy_paste_disable.js
@@ -1,22 +1,22 @@
-url = location.pathname.includes('quizzes')
-if (url == true) {
-  checkBody()
+let url = location.pathname.includes('quizzes');
+if (url === true) {
+  checkBody();
 } else {
-  null
+  null;
 }
 
 function checkBody(){
   console.log('CHECKING BODY');
-  if(document.getElementsByTagName('body').length){
+  if(document.getElementsByTagName('body').length) {
     addTags();
   } else {
     setTimeout(checkBody, 100);
   }
 }
 
-function addTags(){
+function addTags() {
   document.getElementsByTagName('body')[0].setAttribute('oncopy', 'return false');
   document.getElementsByTagName('body')[0].setAttribute('oncut', 'return false');
   document.getElementsByTagName('body')[0].setAttribute('onpaste', 'return false');
-  console.log('TAGS SET')
+  console.log('TAGS SET');
 }
