c03db2bc854e4961482f87b174e06cd4d99cf565
dittonjs <jditton.atomic@gmail.com>
2016-12-21 17:52:21 -0700

    secs

--- client/js/proctor/components/common/not_found.spec.jsx
+++ client/js/proctor/components/common/not_found.spec.jsx
@@ -1,10 +1,11 @@
 import React     from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
+import TestUtils from 'react-addons-test-utils';
 import NotFound  from './not_found';
+import Stub      from '../../../../specs_support/stub';
 
 describe('not found', () => {
   it('renders a "not found" message', () => {
-    const result = TestUtils.renderIntoDocument(<div><NotFound /></div>);
+    const result = TestUtils.renderIntoDocument(<Stub><NotFound /></Stub>);
     const element = TestUtils.findRenderedDOMComponentWithTag(result, 'h1');
     expect(element.textContent).toContain('Page Not Found');
   });
