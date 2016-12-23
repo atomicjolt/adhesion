ce3fc78e910cf952995905f47eebc151d9042652
dittonjs <jditton.atomic@gmail.com>
2016-12-23 10:22:45 -0700

    adds searchable dropdown

--- client/js/proctor/components/exam_distribution/student_assign.jsx
+++ client/js/proctor/components/exam_distribution/student_assign.jsx
@@ -1,30 +1,45 @@
 import React       from 'react';
+import _            from 'lodash';
+import SelectSearch from 'react-select-search';
 import Defines     from '../../defines';
-import CenterSelector from './center_selector.jsx'
 
-export default function studentAssign(props) {
-  const styles = {
-    td: {
-      textAlign: 'left',
-      padding: '20px 20px',
-      borderBottom: `1px solid ${Defines.lightGrey}`
-    },
-  };
+export default class StudentAssign extends React.Component {
+  static propTypes = {
+    student: React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      // id: React.PropTypes.number.isRequired,
+    }),
+    centers: React.PropTypes.arrayOf(React.PropTypes.shape({})),
+  }
 
-  return (
-    <tr>
-      <td style={styles.td}>{props.student.name}</td>
-      <td style={styles.td}><CenterSelector centers={props.centers} /></td>
-      <td style={styles.td}>AssignButton</td>
-      <td style={styles.td}> - </td>
-      <td style={styles.td}> Unchanged </td>
-    </tr>
-  );
+  static getStyles() {
+    return {
+      td: {
+        textAlign: 'left',
+        padding: '20px 20px',
+        borderBottom: `1px solid ${Defines.lightGrey}`,
+        color: Defines.darkGrey,
+      },
+    };
+  }
+
+  getOptions() {
+    return _.map(this.props.centers, center => ({
+      name: center.name,
+      value: center.id,
+    }));
+  }
+
+  render() {
+    const styles = StudentAssign.getStyles();
+    return (
+      <tr>
+        <td style={styles.td}>{this.props.student.name}</td>
+        <td style={styles.td}><SelectSearch options={this.getOptions()} /></td>
+        <td style={styles.td}>AssignButton</td>
+        <td style={styles.td}> - </td>
+        <td style={styles.td}> Unchanged </td>
+      </tr>
+    );
+  }
 }
-
-studentAssign.propTypes = {
-  student: React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    // id: React.PropTypes.number.isRequired,
-  })
-};
