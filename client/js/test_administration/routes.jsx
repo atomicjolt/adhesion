e949c44a846ec61ad89b0637fc4dd0aca10c59af
bensmith <benhsmith13@gmail.com>
2017-01-03 14:58:03 -0700

    Adds all kinds of specs

--- client/js/test_administration/routes.jsx
+++ client/js/test_administration/routes.jsx
@@ -4,7 +4,7 @@ import appHistory                     from '../history';
 import Index                          from './components/index';
 import ExamAssignmentList             from './components/exam_assignment_list/_exam_assignment_list';
 import NotFound                       from './components/common/not_found';
-import PrintTest                      from '../quiz_print/index';
+import PrintTest                      from '../quiz_print/components/index';
 
 export default (
   <Router history={appHistory}>
