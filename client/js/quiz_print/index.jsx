56dcdba5efb7c8925d24cc72965c80a37b7d4ead
bensmith <benhsmith13@gmail.com>
2017-01-03 09:11:34 -0700

    Humble beginnings

---
+++ client/js/quiz_print/index.jsx
@@ -0,0 +1,17 @@
+import React        from 'react';
+
+export default class Index extends React.Component {
+  static propTypes = {
+
+  };
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>Printer Tool</div>
+    );
+  }
+}
