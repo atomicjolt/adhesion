dd4bb6cfd5f8ed70031cf8c7637a26ef7248bac8
David Spencer <davidjspencer@ufl.edu>
2017-01-03 12:10:44 -0700

    Remove column that is never created from being dropped in a migration

--- db/migrate/20161110214431_encrypt.rb
+++ db/migrate/20161110214431_encrypt.rb
@@ -3,6 +3,5 @@ class Encrypt < ActiveRecord::Migration
     add_column :registrations, :encrypted_scorm_cloud_passback_secret_iv, :text
     add_column :registrations, :encrypted_scorm_cloud_passback_secret, :text
     remove_column :registrations, :scorm_cloud_passback_secret
-    remove_column :registrations, :postback_password
   end
 end
