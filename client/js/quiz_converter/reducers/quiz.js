837553b48a56f76d9022f230a5a99b0c038fb132
dspencer001 <dspencer577@gmail.com>
2017-01-11 17:07:24 -0700

    Delete unused reducer

--- client/js/quiz_converter/reducers/quiz.js
+++
@@ -1,9 +0,0 @@
-export default function quiz(state = null, action) {
-  switch (action.type) {
-    case 'CONVERT_QUIZ_DONE':
-      return action.payload;
-
-    default:
-      return state;
-  }
-}
