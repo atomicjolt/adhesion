3942fc66942d1a3334b341261eaa8a66cc787ef3
dittonjs <jditton.atomic@gmail.com>
2017-03-02 16:01:59 -0700

    remove array and key

--- client/js/test_administration/components/exam_request_list/popup_menu.jsx
+++ client/js/test_administration/components/exam_request_list/popup_menu.jsx
@@ -36,9 +36,9 @@ export default function popupMenu(props) {
     outline: 'none'
   } : {};
 
-  let actionButtons;
+  let actionButton;
   if (_.includes(['scheduled', 'paused', 'requested'], props.status)) {
-    actionButtons = (
+    actionButton = (
       <div style={divStyle}>
         <HoverButton
           style={{ ...buttonStyle, ...disabled }}
@@ -50,15 +50,15 @@ export default function popupMenu(props) {
       </div>
     );
   } else {
-    actionButtons = [
-      <div key="popup_pause_button" style={divStyle}>
+    actionButton = (
+      <div style={divStyle}>
         <HoverButton style={buttonStyle} hoveredStyle={hoveredStyle}>Finish</HoverButton>
       </div>
-    ];
+    );
   }
   return (
     <div style={{ ...popupStyle, ...props.style }}>
-      {actionButtons}
+      {actionButton}
       <div style={divStyle}>
         <HoverButton
           style={buttonStyle}
