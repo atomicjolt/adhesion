d8556597d7cf9073a623779b03f802f3b70346cd
KamiJean <kamijeanwilson@gmail.com>
2017-03-13 13:16:34 -0600

    Setting number to 0 if not present

--- client/js/scorm/components/analytics/nav_button.jsx
+++ client/js/scorm/components/analytics/nav_button.jsx
@@ -6,7 +6,7 @@ export default function NavButton(props) {
     <button
       className={ `c-aa-graph-nav__item ${ active }` }
       onClick={ () => props.setActive(props.label) }>
-      <span className="c-aa-stat">{ props.stat }%</span>
+      <span className="c-aa-stat">{ props.stat || 0 }%</span>
       <span className="c-aa-label">{ props.label }</span>
     </button>
   );
