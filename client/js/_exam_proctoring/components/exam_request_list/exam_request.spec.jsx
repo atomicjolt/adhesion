4c45f51d6f0c5021ee89614c8a01af3031b4b246
Francisco Arrieta <francisco.arrieta@aggiemail.usu.edu>
2017-03-14 17:33:14 -0600

    Fix specs (#137)
    * fixing the popupmenu specs
    
    * fixing specs for examrequest
    
    * this fixes most of the warnings
    
    * remove debugger
    
    * Fixing hound
    
    * hound again
    
    * removed comment

--- client/js/_exam_proctoring/components/exam_request_list/exam_request.spec.jsx
+++ client/js/_exam_proctoring/components/exam_request_list/exam_request.spec.jsx
@@ -5,16 +5,47 @@ import ExamRequest                from './exam_request';
 describe('Exam list', () => {
   let result;
   let props;
-
+  let messageSent;
+  let modalHidden;
+  let scheduledExam;
+  let examStatus;
+  let examId;
   beforeEach(() => {
+    messageSent = false;
+    modalHidden = false;
+    scheduledExam = {};
     props = {
       lmsUserId: '1',
       examRequest: {
         student_name: 'Picard',
-        status: 'assigned'
+        student_id: 987,
+        status: 'assigned',
+        id: 123,
+        exam_name: 'starcraft exam',
       },
       openSettings: () => { props.settingsOpen = true; },
       settingsOpen: false,
+      scheduleExam: (date, time, message) => {
+        scheduledExam.date = date;
+        scheduledExam.time = time;
+        scheduledExam.message = message;
+      },
+      showModal: () => {},
+      sendMessage: () => { messageSent = true; },
+      hideModal: () => { modalHidden = true; },
+      examRequestList: [{}],
+      finishExam: (id) => {
+        examStatus = 'finish';
+        examId = id;
+      },
+      enterAnswers: (id) => {
+        examId = id;
+        examStatus = 'ongoing';
+      },
+      startExam: (id) => {
+        examId = id;
+        examStatus = 'start';
+      },
     };
     result = TestUtils.renderIntoDocument(<ExamRequest {...props} />);
   });
@@ -23,5 +54,36 @@ describe('Exam list', () => {
     const button = TestUtils.findRenderedDOMComponentWithTag(result, 'button');
     TestUtils.Simulate.click(button);
     expect(props.settingsOpen).toBe(true);
+  });
+
+  it('schedule an exam', () => {
+    const selectedTime = {
+      value: 'seconds since 1970'
+    };
+    result.scheduleExam('date is 2099', selectedTime, 'my man');
+    expect(messageSent).toBeTruthy();
+    expect(modalHidden).toBeTruthy();
+    expect(scheduledExam).toBeDefined();
+    expect(scheduledExam).toEqual(jasmine.objectContaining({
+      message: 'seconds since 1970'
+    }));
+  });
+
+  it('finish exam', () => {
+    result.finishExam(props.examRequest.id);
+    expect(examStatus).toContain('finish');
+    expect(examId).toEqual(props.examRequest.id);
+  });
+
+  it('enter answer', () => {
+    result.enterAnswers();
+    expect(examStatus).toContain('ongoing');
+    expect(examId).toEqual(props.examRequest.id);
+  });
+
+  it('start exam', () => {
+    result.startExam();
+    expect(examStatus).toContain('start');
+    expect(examId).toEqual(props.examRequest.id);
   });
 });
