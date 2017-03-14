68554f0a807347b84f78276b65481b2303aa385e
Francisco <fjab92@gmail.com>
2017-03-14 15:18:22 -0600

    remove debugger

--- client/js/_exam_proctoring/components/exam_request_list/_exam_request_list.jsx
+++ client/js/_exam_proctoring/components/exam_request_list/_exam_request_list.jsx
@@ -166,7 +166,6 @@ export class BaseExamRequestList extends React.Component {
     }
 
     renderList = this.tabFilter(renderList);
-    debugger;
     return _.map(renderList, examRequest => (
       <ExamRequest
         key={`proctor_${examRequest.id}`}
