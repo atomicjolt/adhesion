db79095aaf468bfd61344cb781c81549be2d6ef2
Brady Riddle <brady.riddle@atomicjolt.com>
2017-03-20 17:48:58 -0600

    Made attendance icons selectable by keypress

--- client/js/attendance/components/main/student.jsx
+++ client/js/attendance/components/main/student.jsx
@@ -43,10 +43,15 @@ export default class Student extends React.Component {
               aria-setsize="3"
               aria-posinset="1"
               aria-label="present"
-              tabIndex="0"
             >
-              <label className="c-present" htmlFor={`radio1_${id}`}>
+              <label
+                className="c-present"
+                htmlFor={`radio1_${id}`}
+                tabIndex="0"
+                onKeyPress={() => this.updateStatus(student, status, AttendanceStates.PRESENT)}
+              >
                 <input
+                  tabIndex="0"
                   type="radio"
                   name={`radio1_${id}`}
                   id={`radio1_${id}`}
@@ -62,9 +67,13 @@ export default class Student extends React.Component {
               aria-setsize="3"
               aria-posinset="2"
               aria-label="late"
-              tabIndex="0"
             >
-              <label className="c-late" htmlFor={`radio2_${id}`}>
+              <label
+                className="c-late"
+                htmlFor={`radio2_${id}`}
+                tabIndex="0"
+                onKeyPress={() => this.updateStatus(student, status, AttendanceStates.LATE)}
+              >
                 <input
                   type="radio"
                   name={`radio1_${id}`}
@@ -81,9 +90,13 @@ export default class Student extends React.Component {
               aria-setsize="3"
               aria-posinset="3"
               aria-label="absent"
-              tabIndex="0"
             >
-              <label className="c-absent" htmlFor={`radio3_${id}`}>
+              <label
+                className="c-absent"
+                htmlFor={`radio3_${id}`}
+                tabIndex="0"
+                onKeyPress={() => this.updateStatus(student, status, AttendanceStates.ABSENT)}
+              >
                 <input
                   type="radio"
                   name={`radio1_${id}`}
