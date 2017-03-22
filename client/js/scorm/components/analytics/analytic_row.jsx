c385fbdbc7d856797c6cf9e201525b8e7482d977
KamiJean <kamijeanwilson@gmail.com>
2017-03-22 17:33:31 -0600

    Getting the arrow icon to rotate

--- client/js/scorm/components/analytics/analytic_row.jsx
+++ client/js/scorm/components/analytics/analytic_row.jsx
@@ -1,66 +1,85 @@
 import React  from 'react';
 import _ from 'lodash';
 
-export default function AnalyticRow(props) {
+export class AnalyticRow extends React.Component {
 
-  function formatScore(score) {
+  static propTypes = {
+    id: React.PropTypes.number,
+    name: React.PropTypes.string,
+    passed: React.PropTypes.string,
+    score: React.PropTypes.number,
+    time: React.PropTypes.number,
+    isParent: React.PropTypes.bool,
+    depth: React.PropTypes.number,
+    switchTable: React.PropTypes.func.isRequired,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      isOpen: true,
+    };
+  }
+
+  formatScore(score) {
     if (score || score === 0) {
       return `${score * 100}%`;
     }
     return 'N/A';
   }
 
-  function formatTime(time) {
+  formatTime(time) {
     return _.ceil(time / 60);
   }
 
-  let rowStyles = {};
-  let iconStyles = {};
-  let firstColumnStyle = {};
-  let tableClass = 'c-aa-row';
-  let icon;
-  if (props.depth) {
-    const totalDepth = 2 * props.depth;
-    const iconLeft = totalDepth + 1.3;
-    const rowLeft = totalDepth + 4;
-    iconStyles = {
-      left: `${iconLeft}rem`,
-    };
-    firstColumnStyle = {
-      paddingLeft: `${rowLeft}rem`,
-    };
-  }
-  if (props.isParent || !(props.depth >= 0)) {
-    rowStyles = {
-      cursor: 'pointer',
-    };
-    if (props.isParent) {
-      tableClass += ' c-aa-accordion is-open';
-      icon = <i style={iconStyles} className="material-icons">arrow_drop_down</i>;
+  rowClicked(viewId) {
+    if (!(this.props.depth >= 0) && viewId) {
+      this.props.switchTable(viewId);
+    } else if (this.props.isParent) {
+      this.setState({ isOpen: !this.state.isOpen });
     }
   }
 
-  return (
-    <tr
-      className={tableClass}
-      onClick={() => props.switchTable(props.id)}
-      style={rowStyles}
-    >
-      <td style={firstColumnStyle}>{icon}{props.name || 'Unknown'}</td>
-      <td>{props.passed}</td>
-      <td>{formatScore(props.score)}</td>
-      <td>{formatTime(props.time)}</td>
-    </tr>
-  );
+  render() {
+    let rowStyles = {};
+    let iconStyles = {};
+    let firstColumnStyle = {};
+    let tableClass = 'c-aa-row';
+    let icon;
+    if (this.props.depth) {
+      const totalDepth = 2 * this.props.depth;
+      const iconLeft = totalDepth + 1.3;
+      const rowLeft = totalDepth + 4;
+      iconStyles = {
+        left: `${iconLeft}rem`,
+      };
+      firstColumnStyle = {
+        paddingLeft: `${rowLeft}rem`,
+      };
+    }
+    if (this.props.isParent || !(this.props.depth >= 0)) {
+      rowStyles = {
+        cursor: 'pointer',
+      };
+      if (this.props.isParent) {
+        tableClass += ` c-aa-accordion ${this.state.isOpen ? 'is-open' : ''}`;
+        icon = <i style={iconStyles} className="material-icons">arrow_drop_down</i>;
+      }
+    }
+
+    return (
+      <tr
+        className={tableClass}
+        onClick={() => this.rowClicked(this.props.id)}
+        style={rowStyles}
+      >
+        <td style={firstColumnStyle}>{icon}{this.props.name || 'Unknown'}</td>
+        <td>{this.props.passed}</td>
+        <td>{this.formatScore(this.props.score)}</td>
+        <td>{this.formatTime(this.props.time)}</td>
+      </tr>
+    );
+  }
 }
 
-AnalyticRow.propTypes = {
-  id: React.PropTypes.number,
-  name: React.PropTypes.string,
-  passed: React.PropTypes.string,
-  score: React.PropTypes.number,
-  time: React.PropTypes.number,
-  isParent: React.PropTypes.bool,
-  depth: React.PropTypes.number,
-  switchTable: React.PropTypes.func.isRequired,
-};
+export default AnalyticRow;
