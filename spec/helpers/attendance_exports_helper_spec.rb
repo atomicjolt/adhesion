65c9fc76209f98e17ee75fa4ea3b2a3cde3c4a89
Francisco <fjab92@gmail.com>
2017-03-24 11:12:24 -0600

    Fixing spec

--- spec/helpers/attendance_exports_helper_spec.rb
+++ spec/helpers/attendance_exports_helper_spec.rb
@@ -3,13 +3,9 @@ require "spec_helper"
 describe AttendanceExportsHelper do
   before :each do
     @attendance = FactoryGirl.create(:attendance)
-    @student = {}
-    @student["id"] = 1
-    @student["sortable_name"] = "Batman"
-    @student["name"] = "Batman"
   end
   it "Builds the CSV Correctly" do
-    csv = AttendanceExportsHelper.generate_csv([@student], [@attendance])
+    csv = AttendanceExportsHelper.generate_csv([@attendance])
     expect(csv).to eq("Name,2016-12-08\nBatman,present\n")
   end
 end
