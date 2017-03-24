365f0de43a6a0820c14f9ca3e88c13288170b334
Francisco Arrieta <francisco.arrieta@aggiemail.usu.edu>
2017-03-24 15:56:56 -0600

    Fixing the attend export (#152)
    * Fixing the attend export
    
    * fix whitespace
    
    * Fixing spec
    
    * Fixed spec

--- app/helpers/attendance_exports_helper.rb
+++ app/helpers/attendance_exports_helper.rb
@@ -1,23 +1,26 @@
 module AttendanceExportsHelper
-  def self.generate_csv(students, attendances)
+  def self.generate_csv(attendances)
     CSV.generate do |csv|
       days = attendances.group_by(&:date).keys.sort
       header_row = ["Name"].concat days
       csv << header_row
-      sorted_students = students.sort_by { |s| s["sortable_name"] }
+
+      sorted_students = attendances.sort_by { |s| s.sortable_name.downcase }
+      sorted_students = sorted_students.map(&:name).uniq
+
       sorted_students.each do |student|
-        if student["name"].include?(",")
-          parts = student["name"].split(",")
+        if student.include?(",")
+          parts = student.split(",")
           parts = parts.map(&:strip)
           last = parts.shift
           row = [parts.join(" ") << " " << last]
         else
-          row = [student["name"]]
+          row = [student]
         end
 
         days.each do |day|
           att = attendances.detect do |attendance|
-            attendance.date == day && student["id"] == attendance.lms_student_id
+            attendance.name == student && attendance.date == day
           end
 
           row << if att.present? && att.status.present?
