ff76233f66db437f7aa5f26ae02b8679761028e7
Francisco <fjab92@gmail.com>
2017-03-24 15:46:04 -0600

    Fixed spec

--- spec/helpers/attendance_exports_helper_spec.rb
+++ spec/helpers/attendance_exports_helper_spec.rb
@@ -6,6 +6,6 @@ describe AttendanceExportsHelper do
   end
   it "Builds the CSV Correctly" do
     csv = AttendanceExportsHelper.generate_csv([@attendance])
-    expect(csv).to eq("Name,2016-12-08\nBatman,present\n")
+    expect(csv).to eq("Name,2016-12-08\n" + @attendance.name + ",present\n")
   end
 end
