570bd9d92737223a8c4a359c8c709e56757b54fd
Francisco Arrieta <francisco.arrieta@aggiemail.usu.edu>
2017-03-31 11:41:33 -0600

    The esc key will now close the export modal (#159)
    * The esc key will now close the export modal
    
    * fixing hound issues
    
    * fix space
    
    * put some sugar on the syntax style

--- client/js/attendance/components/main/student_list.jsx
+++ client/js/attendance/components/main/student_list.jsx
@@ -45,6 +45,8 @@ export class StudentList extends React.Component {
 
   constructor() {
     super();
+
+    this.handleKeyPress = ::this.handleKeyPress;
     this.state = {
       showExportModal: false,
     };
@@ -58,6 +60,20 @@ export class StudentList extends React.Component {
     };
     this.props.canvasRequest(listUsersInCourseUsers, params, {});
     this.props.getStudentAttendance(this.props.applicationDate, this.props.settings.lms_course_id);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {
+    if (event.keyCode === 27) {
+      this.setState({ showExportModal: false });
+    }
   }
 
   updateStudentAttendance(student, status) {
