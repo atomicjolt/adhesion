632ab9732aa84b7a0f5853c3bf68948f1ee38a13
James Carbine <JamesCarbine@gmail.com>
2017-04-03 11:36:41 -0600

    Refactor scorm engine service to use RestClient (#162)
    * Refactor scorm engine service to use RestClient
    Change resultsFormat to return FULL in the postback
    Use interpolation for the urls
    Add proxy setting to RestClient in an initializer
    
    * Cache service init

--- app/helpers/scorm_course_helper.rb
+++ app/helpers/scorm_course_helper.rb
@@ -1,9 +1,9 @@
 module ScormCourseHelper
   def scorm_connect_service
-    if current_application_instance.scorm_type == "engine"
-      ScormEngineService.new
-    else
-      ScormCloudService.new
-    end
+    @service ||= if current_application_instance.scorm_type == "engine"
+                   ScormEngineService.new
+                 else
+                   ScormCloudService.new
+                 end
   end
 end
