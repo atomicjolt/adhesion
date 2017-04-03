ecdba6394ec8611d3080c724339eec05a3cd18ac
KamiJean <kamijeanwilson@gmail.com>
2017-04-03 15:35:33 -0600

    Removing the default from the lms_course_id

--- app/helpers/scorm_course_helper.rb
+++ app/helpers/scorm_course_helper.rb
@@ -1,5 +1,5 @@
 module ScormCourseHelper
-  def scorm_connect_service(lms_course_id = "default")
+  def scorm_connect_service(lms_course_id)
     @service ||= if current_application_instance.scorm_type == "engine"
                    ScormEngineService.new(lms_course_id)
                  else
