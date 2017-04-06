5d195ddac1c4b82561af2e470d7e5cae567a281a
James Carbine <jamescarbine@gmail.com>
2017-04-06 16:49:14 -0600

    Add a delete confirmation to delete a package in scorm.

---
+++ client/js/scorm/components/scorm/confirm_delete.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Defines from '../../../defines';
+import HoverButton from '../common/hover_button';
+
+const confirmDelete = (props) => {
+  const styles = {
+    popupStyle: {
+      boxShadow: `0px 0px 5px ${Defines.darkGrey}`,
+      backgroundColor: 'white',
+      padding: '25px',
+      position: 'fixed',
+      top: '5vh',
+      left: '25vw',
+      right: '25vw',
+      zIndex: '2',
+      color: Defines.darkGrey,
+    },
+    buttonStyle: {
+      backgroundColor: Defines.tanishBrown,
+      color: 'white',
+      border: 'none',
+      padding: '10px 20px',
+      cursor: 'pointer',
+      float: 'right',
+    },
+    cancelButton: {
+      backgroundColor: Defines.lightBackground,
+      color: Defines.darkGrey,
+      marginRight: '20px'
+    },
+    buttonContainer: {
+      marginTop: '20px'
+    },
+  };
+
+  return (
+    <div style={styles.popupStyle}>
+      <h2>Are you sure?</h2>
+      <div style={styles.buttonContainer}>
+        <HoverButton
+          style={styles.buttonStyle}
+          onClick={props.handleRemove}
+        >
+          DELETE
+        </HoverButton>
+        <HoverButton
+          style={{ ...styles.buttonStyle, ...styles.cancelButton }}
+          onClick={props.closeModal}
+        >
+          CANCEL
+        </HoverButton>
+      </div>
+    </div>
+  );
+};
+
+confirmDelete.propTypes = {
+  closeModal: React.PropTypes.func.isRequired,
+  handleRemove: React.PropTypes.func.isRequired,
+};
+
+export default confirmDelete;
