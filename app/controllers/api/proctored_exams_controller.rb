b280b4905539f3cebc90aa2a207c673bfcd6300d
dittonjs <jditton.atomic@gmail.com>
2017-06-20 08:48:39 -0600

    adds auto finish

--- app/controllers/api/proctored_exams_controller.rb
+++ app/controllers/api/proctored_exams_controller.rb
@@ -1,10 +1,16 @@
 class Api::ProctoredExamsController < ApplicationController
-  before_action :validate_proctor_code
+  before_action :validate_proctor_code, only: [:start_proctored_exam]
+  skip_before_action :validate_token
+  skip_before_action :protect_from_forgery
   include Concerns::CanvasSupport
 
   respond_to :json
 
   def start_proctored_exam
+    if params[:update]
+      finish_proctored_exam
+      return
+    end
     quiz_params = {
       id: @exam_request.exam_id,
       course_id: @exam_request.course_id,
@@ -14,11 +20,18 @@ class Api::ProctoredExamsController < ApplicationController
     render json: { quiz: @exam_request, proctor_access_code: quiz.parsed_response["access_code"] }
   end
 
+  def finish_proctored_exam
+    @exam_request.update(status: "finished")
+    render json: { status: "ok" }
+  end
+
   private
 
   def validate_proctor_code
     @exam_request = ExamRequest.find_by(student_id: params[:student_id], status: "started")
-
+    if params[:update]
+      return
+    end
     if !@exam_request
       render json: { error: "You do not have an exam that is ready to start." }
       return
@@ -38,6 +51,9 @@ class Api::ProctoredExamsController < ApplicationController
           user_id: user["id"],
         }
         proctor_code = canvas_api.proxy("LOAD_CUSTOM_DATA", custom_data_params).parsed_response["data"]
+        puts "********************************"
+        puts proctor_code
+        puts "********************************"
         if proctor_code == params[:proctor_code]
           @exam_request.update_attributes(unlocked_by_id: user["id"], unlocked_by_name: user["name"])
           matched_code = true
