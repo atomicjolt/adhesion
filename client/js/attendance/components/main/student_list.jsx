43341f71095ea54ead0cb411a2c0bba49d51026e
Bruce Amberson <bruce.amberson@atomicjolt.com>
2017-09-08 16:49:57 -0600

    No string literals

--- client/js/attendance/components/main/student_list.jsx
+++ client/js/attendance/components/main/student_list.jsx
@@ -198,7 +198,7 @@ export class StudentList extends React.Component {
           <button
             className="c-btn c-btn--mark-all"
             onClick={() => this.markAll(AttendanceStates.PRESENT)}
-            ref="mark_all"
+            ref={"mark_all"}
           >
             Mark All Present
           </button>
