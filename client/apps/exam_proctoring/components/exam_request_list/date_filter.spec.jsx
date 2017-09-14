36ce6c6ac7e3250a84b3c735d3e43f059f40707e
Bruce Amberson <bruce.amberson@atomicjolt.com>
2017-09-14 17:21:08 -0600

    One last change to specs.

--- client/apps/exam_proctoring/components/exam_request_list/date_filter.spec.jsx
+++ client/apps/exam_proctoring/components/exam_request_list/date_filter.spec.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import { shallow } from 'enzyme';
 import hover from '../common/hover_button';
+import datePicker from '../common/date_selector';
 import DateFilter from './date_filter';
 
 describe('Date Selector', () => {
@@ -18,6 +19,10 @@ describe('Date Selector', () => {
 
   it('Renders two hover buttons', () => {
     expect(result.find(hover).length).toEqual(2);
+  });
+
+  it('Renders two datePicker buttons', () => {
+    expect(result.find(datePicker).length).toEqual(1);
   });
 
   // it('matches the snapshot', () => {
