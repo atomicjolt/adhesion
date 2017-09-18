8bd26a3f17a7ca6dbddc20d325abdb4ee06ba222
James Carbine <jamescarbine@gmail.com>
2017-09-18 09:12:51 -0600

    Update secrets init

--- config/initializers/nuclear_secrets.rb
+++ config/initializers/nuclear_secrets.rb
@@ -1,3 +1,4 @@
+# rubocop:disable BlockLength
 NuclearSecrets.configure do |config|
   config.required_secrets = {
     admin_name: String,
@@ -15,9 +16,21 @@ NuclearSecrets.configure do |config|
     canvas_developer_key: String,
     auth0_client_id: String,
     auth0_client_secret: String,
-    hello_world_lti_secret: String,
-    admin_lti_secret: String,
     secret_key_base: String,
     secret_token: NilClass,
+    canvas_proctor_url: NilClass,
+    scorm_url: String,
+    scorm_api_path: String,
+    scorm_api_username: String,
+    scorm_api_password: String,
+    proctor_login_secret: String,
+    scorm_lti_secret: String,
+    scorm_analytics_lti_secret: String,
+    attendance_lti_secret: String,
+    quiz_converter_lti_secret: String,
+    test_lti_secret: String,
+    proctor_tool_lti_secret: String,
+    scorm_cloud_app_id: String,
+    scorm_cloud_secret_key: String,
   }
 end
