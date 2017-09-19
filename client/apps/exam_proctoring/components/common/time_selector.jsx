bf5333fe665acf68387bf975aebe5f67b6c3a2c7
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2017-09-19 17:37:07 -0600

    hound

--- client/apps/exam_proctoring/components/common/time_selector.jsx
+++ client/apps/exam_proctoring/components/common/time_selector.jsx
@@ -13,10 +13,10 @@ export default class TimeSelector extends React.Component {
   static getTimes() {
     const times = [];
     for (let i = 0; i < 24; i += 1) {
-      times.push(i + '00');
-      times.push(i + '15');
-      times.push(i + '30');
-      times.push(i + '45');
+      times.push(`${i}00`);
+      times.push(`${i}15`);
+      times.push(`${i}30`);
+      times.push(`${i}45`);
     }
     return _.map(times, (time) => {
       if (time.length === 3) {
