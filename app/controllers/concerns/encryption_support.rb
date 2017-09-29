3be5ed96c3063d207be6c48c18b38aae59699c18
James Carbine <jamescarbine@gmail.com>
2017-09-29 13:54:10 -0600

    Add Encryption Support Treat keys as hex-encoded byte strings so that we can have a full 256 bits of entropy Update secret example

---
+++ app/controllers/concerns/encryption_support.rb
@@ -0,0 +1,11 @@
+module Concerns
+  module EncryptionSupport
+    extend ActiveSupport::Concern
+
+    protected
+
+    def decode_hex(str)
+      @bytes ||= str.scan(/../).map { |x| x.hex.chr }.join
+    end
+  end
+end
