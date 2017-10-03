370bd03e47f6e4bdded40bcf06fbfbc7dd33db5f
dittonjs <jditton.atomic@gmail.com>
2017-10-03 14:21:40 -0600

    updates to use secrets

--- app/lib/integrations/u4sm.rb
+++ app/lib/integrations/u4sm.rb
@@ -1,39 +1,39 @@
 require "rest-client"
 
 module Integrations
-  
-    module U4sm
-      def self.post_grades_to_sis(sis_course_id, sis_section_id, gradetype, grades)
-        cookie = U4sm.get_cookie
-        return 500 if cookie.empty? 
-        resp = RestClient.post(
-          "https://au.adminportal-dev.af.edu/U4SMAPI/Canvas/CanvasGradePush",
-          {
-            sis_course_id: sis_course_id,
-            sis_section_id: sis_section_id,
-            gradetype: gradetype,
-            grades: grades,
+
+  module U4sm
+    def self.post_grades_to_sis(sis_course_id, sis_section_id, gradetype, grades)
+      cookie = U4sm.get_cookie
+      return 500 if cookie.empty?
+      resp = RestClient.post(
+        "#{Rails.application.secrets.u4sm_url}/U4SMAPI/Canvas/CanvasGradePush",
+        {
+          sis_course_id: sis_course_id,
+          sis_section_id: sis_section_id,
+          gradetype: gradetype,
+          grades: grades,
+        },
+        {
+          content_type: "application/json",
+          cookies: {
+            ".MosaicAuthorization": cookie,
           },
-          {
-            content_type: "application/json",
-            cookies: {
-              ".MosaicAuthorization": cookie,
-            },
-          },
-        )
-        resp.code
-      end
-      
-      def self.get_cookie
-        resp = RestClient.post(
-          "https://au.adminportal-dev.af.edu/U4SMAPI/api/authentication/login",
-          {
-            username: "", #TODO figure out what this actually needs to be
-            password: ""
-          },
-        )
-        resp.cookies[".MosaicAuthorization"]
-      end
+        },
+      )
+      resp.code
     end
-  
+
+    def self.get_cookie
+      resp = RestClient.post(
+        "#{Rails.application.secrets.u4sm_url}/U4SMAPI/api/authentication/login",
+        {
+          username: Rails.application.secrets.u4sm_username,
+          password: Rails.application.secrets.u4sm_password,
+        },
+      )
+      resp.cookies[".MosaicAuthorization"]
+    end
   end
+
+end
