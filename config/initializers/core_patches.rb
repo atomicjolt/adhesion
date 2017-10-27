adda9d6b67cf2b5c5cb3e63081661cc9bccfda62
Justin Ball <justinball@gmail.com>
2017-10-27 16:38:07 -0600

    Adds more comments

--- config/initializers/core_patches.rb
+++ config/initializers/core_patches.rb
@@ -1,1 +1,4 @@
+# Monkey patch the Uri Generic class find_proxy method
+# We are removing code that attempts to resolve a host name
+# without using the proxy so that the request doesn't time out.
 URI::Generic.prepend CoreExtensions::Uri::Generic
