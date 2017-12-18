19e3fa6b08ea1b97cf9382fc74d4e0e9ec352d67
James Carbine <jamescarbine@gmail.com>
2017-12-18 16:08:52 -0700

    Reduce the buffer from 100 MB to 1 MB

--- config/initializers/rack.rb
+++ config/initializers/rack.rb
@@ -2,8 +2,4 @@
 # https://github.com/rack/rack/issues/1075
 # The middleware approach was not reliable.
 # Setting the const, in my tests, is always reliable.
-# This makes large file uploads process MUCH faster
-# The new rack code sets this to 1_048_576 so I think this
-# hack is still a better approach for this scenario with
-# huge scorm files.
-Rack::Multipart::Parser.const_set("BUFSIZE", 100 * 1024 * 1024)
+Rack::Multipart::Parser.const_set("BUFSIZE", 1024 * 1024)
