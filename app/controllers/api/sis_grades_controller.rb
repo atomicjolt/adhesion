9372b3b5fde47f7b7fb137d6b78a27f866a46640
James Carbine <jamescarbine@gmail.com>
2018-01-17 11:24:03 -0700

    Protect sis_grades api with a shared auth jwt

--- app/controllers/api/sis_grades_controller.rb
+++ app/controllers/api/sis_grades_controller.rb
@@ -1,6 +1,9 @@
 class Api::SisGradesController < Api::ApiApplicationController
   respond_to :json
 
+  skip_before_action :validate_token, only: %i[index]
+  before_action :validate_token_shared, only: %i[index]
+
   def index
     grade = SisGrade.find_by(
       sis_course_id: params[:sis_course_id],
@@ -12,6 +15,18 @@ class Api::SisGradesController < Api::ApiApplicationController
       render json: grade.grades
     else
       raise ActiveRecord::RecordNotFound
+    end
+  end
+
+  private
+
+  def validate_token_shared
+    if params[:shared_auth] == true || params[:shared_auth] == "true"
+      aud = Rails.application.secrets.auth0_client_id
+      secret = Rails.application.secrets.shared_sis_auth_secret
+      validate_token_with_secret(aud, secret)
+    else
+      validate_token
     end
   end
 end
