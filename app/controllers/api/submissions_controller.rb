329ce4c25fb8d42d241a6dc20f385942422ef450
James Carbine <jamescarbine@gmail.com>
2018-02-01 13:53:24 -0700

    Refactor submissions api and make methods a bit more compact.

--- app/controllers/api/submissions_controller.rb
+++ app/controllers/api/submissions_controller.rb
@@ -3,34 +3,51 @@ class Api::SubmissionsController < Api::ApiApplicationController
   include Concerns::CanvasSupport
 
   def create
-    submissions = []
-
-    params[:sections].each do |section|
-      if section["id"].to_i > 0
-        sub_params = {
-          section_id: section["id"],
-          assignment_id: params[:assignment_id],
-        }
-        if params[:assignment_id] != "total"
-          section_subs = canvas_api.proxy("LIST_ASSIGNMENT_SUBMISSIONS_SECTIONS", sub_params)
-        end
-        section_users = canvas_api.proxy("LIST_ENROLLMENTS_SECTIONS", { section_id: section["id"] })
-        submissions.append({ subs: section_subs, users: section_users, section: section })
-      end
-    end
+    submissions = extract_submissions(params[:sections])
 
     submissions.each do |section_info|
-      if params[:assignment_id] == "total"
-        grades = total_grades(section_info[:users])
-      else
-        grades = make_grades(section_info)
-      end
       Integrations::SIS.post_grades_to_db(
         section_info[:section][:sis_course_id],
         section_info[:section][:sis_section_id],
         params[:type],
-        grades,
+        grades(section_info),
       )
+    end
+  end
+
+  def extract_submissions(sections)
+    sections.select do |sec|
+      sec["id"].to_i > 0
+    end.map do |section|
+      if params[:assignment_id] != "total"
+        sub_params = {
+          section_id: section["id"],
+          assignment_id: params[:assignment_id],
+        }
+        section_subs = canvas_api.proxy(
+          "LIST_ASSIGNMENT_SUBMISSIONS_SECTIONS",
+          sub_params,
+        )
+      end
+
+      section_users = canvas_api.proxy(
+        "LIST_ENROLLMENTS_SECTIONS",
+        { section_id: section["id"] },
+      )
+
+      {
+        subs: section_subs,
+        users: section_users,
+        section: section,
+      }
+    end
+  end
+
+  def grades(section_info)
+    if params[:assignment_id] == "total"
+      total_grades(section_info[:users])
+    else
+      make_grades(section_info)
     end
   end
 
@@ -56,8 +73,7 @@ class Api::SubmissionsController < Api::ApiApplicationController
 
   def get_user_sis(users, user_id)
     if user = users.detect { |u| u["user_id"] == user_id }
-      return user["sis_user_id"]
+      user["sis_user_id"]
     end
-    nil
   end
 end
