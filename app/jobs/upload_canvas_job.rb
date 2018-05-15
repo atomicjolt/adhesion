96cc1930c40f43b857dc283e67e7d390c4df1180
James Carbine <jamescarbine@gmail.com>
2018-05-15 09:10:38 -0600

    Improve scorm canvas file upload Find and create the scorm_files directory as a locked folder(this will require an update to lms-api gem, to fix the RESOLVE_PATH_COURSES_FULL_PATH call to send the path to canvas) Upload the file as a locked file.

--- app/jobs/upload_canvas_job.rb
+++ app/jobs/upload_canvas_job.rb
@@ -3,6 +3,9 @@ class UploadCanvasJob < ApplicationJob
   include ScormCourseHelper
   queue_as :default
 
+  SCORM_DIR = "scorm_files".freeze
+  CANVAS_ROOT_DIR = "course files".freeze
+
   def perform(
     application_instance,
     current_user,
@@ -44,7 +47,18 @@ class UploadCanvasJob < ApplicationJob
 
   def process_canvas_file(scorm_course, file_path, lms_course_id)
     delete_canvas_file(scorm_course.file_id) if scorm_course&.file_id
-    file_id = upload_canvas_file(file_path, lms_course_id)
+    file_id =
+      if file_path.present?
+        folder = get_folder(lms_course_id)
+        if folder.nil?
+          folder = create_folder(lms_course_id)
+        end
+        if folder["locked"] != true
+          update_folder(folder)
+        end
+        upload_canvas_file(file_path, lms_course_id, folder)
+      end
+
     if file_id
       hide_scorm_file(file_id)
       scorm_course.update(
@@ -56,40 +70,81 @@ class UploadCanvasJob < ApplicationJob
     end
   end
 
-  def upload_canvas_file(file_path, lms_course_id)
-    if file_path.present?
-      canvas_response = @canvas_api.proxy(
-        "COURSES_UPLOAD_FILE",
-        {
-          course_id: lms_course_id,
-        },
-        {
-          name: File.basename(file_path),
-          content_type: "application/zip",
-          parent_folder_path: "scorm_files/",
-          on_duplicate: "rename",
-        },
-      ).parsed_response
-      canvas_response["upload_params"]["file"] =
-        UploadIO.new(File.new(file_path), "application/zip", File.basename(file_path))
+  def get_folder(lms_course_id)
+    folders = @canvas_api.proxy(
+      "RESOLVE_PATH_COURSES_FULL_PATH",
+      {
+        course_id: lms_course_id,
+        path: SCORM_DIR,
+      },
+    ).parsed_response
 
-      RestClient.post(
-        canvas_response["upload_url"],
-        canvas_response["upload_params"],
-      ) do |response|
-        case response.code
-        when 200
-          JSON.parse(response.body)["id"]
-        when 302, 303
-          file_confirm = RestClient.get(response.headers[:location])
-          JSON.parse(file_confirm.body)["id"]
-        end
+    folders.detect do |folder|
+      folder["full_name"] == "#{CANVAS_ROOT_DIR}/#{SCORM_DIR}"
+    end
+  rescue LMS::Canvas::InvalidAPIRequestFailedException => e
+    raise e unless e.to_s.include?("Status: 404 Not Found")
+  end
+
+  def update_folder(folder)
+    @canvas_api.proxy(
+      "UPDATE_FOLDER",
+      {
+        id: folder["id"],
+      },
+      {
+        locked: true,
+      },
+    ).parsed_response
+  end
+
+  def create_folder(lms_course_id)
+    @canvas_api.proxy(
+      "CREATE_FOLDER_COURSES",
+      {
+        course_id: lms_course_id,
+      },
+      {
+        name: SCORM_DIR,
+        parent_folder_path: CANVAS_ROOT_DIR,
+        locked: true,
+      },
+    ).parsed_response
+  end
+
+  def upload_canvas_file(file_path, lms_course_id, folder)
+    canvas_response = @canvas_api.proxy(
+      "COURSES_UPLOAD_FILE",
+      {
+        course_id: lms_course_id,
+      },
+      {
+        name: File.basename(file_path),
+        content_type: "application/zip",
+        parent_folder_id: folder["id"],
+        on_duplicate: "rename",
+        locked: true,
+      },
+    ).parsed_response
+    canvas_response["upload_params"]["file"] =
+      UploadIO.new(File.new(file_path), "application/zip", File.basename(file_path))
+
+    RestClient.post(
+      canvas_response["upload_url"],
+      canvas_response["upload_params"],
+    ) do |response|
+      case response.code
+      when 200
+        JSON.parse(response.body)["id"]
+      when 302, 303
+        file_confirm = RestClient.get(response.headers[:location])
+        JSON.parse(file_confirm.body)["id"]
       end
     end
   end
 
   def hide_scorm_file(file_id)
-    @canvas_api.proxy("UPDATE_FILE", { id: file_id }, { hidden: true })
+    @canvas_api.proxy("UPDATE_FILE", { id: file_id }, { locked: true })
   end
 
   def delete_canvas_file(file_id)
