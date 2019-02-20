fc068611a56553a1e8bc9da557669a8c102233d5
James Carbine <jamescarbine@gmail.com>
2019-02-20 11:38:19 -0700

    Upgrade gems
    Upgrade gems to match deployed version
    Fix spec factories to be compatible with new factory_bot

--- spec/factories/sis_grades.rb
+++ spec/factories/sis_grades.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     sis_course_id { generate(:sis_course_id) }
     sis_section_id { generate(:sis_section_id) }
     gradetype { generate(:gradetype) }
-    grades []
+    grades { [] }
   end
 end
