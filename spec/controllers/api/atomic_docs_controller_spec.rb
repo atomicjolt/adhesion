6b5062d568f3633a1f6c5136f6aeb3f468de8600
James Carbine <jamescarbine@gmail.com>
2019-05-13 14:54:23 -0600

    Update specs

--- spec/controllers/api/atomic_docs_controller_spec.rb
+++ spec/controllers/api/atomic_docs_controller_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Api::AtomicDocsController, type: :controller do
+  include ActiveJob::TestHelper
+
+  after do
+    clear_enqueued_jobs
+  end
+
   context "no api token" do
     describe "POST documents" do
       it "returns unauthorized" do
@@ -22,9 +28,47 @@ RSpec.describe Api::AtomicDocsController, type: :controller do
       @api_token = create(:api_token, name: "atomic-doc")
 
       request.headers["Authorization"] = @api_token.token
+
+      @url = "www.example.com/sha"
     end
 
     describe "POST documents" do
+      it "returns authorized" do
+        params = {
+          url: @url,
+        }
+        post :documents, params: params
+        expect(response).to have_http_status(200)
+      end
+
+      it "finds the existing document and returns that" do
+        atomic_doc = create(:atomic_doc, url: @url, status: "queued")
+        params = {
+          url: @url,
+        }
+        post :documents, params: params
+        body = JSON.parse(response.body)
+        expect(body["id"]).to eq(atomic_doc.id)
+        expect(body["status"]).to eq("queued")
+      end
+
+      it "enqueues an AtomicDocJob" do
+        params = {
+          url: @url,
+        }
+        expect do
+          post :documents, params: params
+        end.to have_enqueued_job(AtomicDocJob)
+      end
+
+      it "enqueues an AtomicDocJob with data" do
+        atomic_doc = create(:atomic_doc, url: @url)
+        params = {
+          url: @url,
+        }
+        expect(AtomicDocJob).to receive(:perform_later).with(atomic_doc)
+        post :documents, params: params
+      end
     end
 
     describe "POST sessions" do
