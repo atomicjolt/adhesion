948890635a29e9f7b8b6b76dc535dfa71f11c660
James Carbine <jamescarbine@gmail.com>
2019-05-14 09:40:53 -0600

    DRY up the reducer a little

--- client/apps/atomic_docs/reducers/application.js
+++ client/apps/atomic_docs/reducers/application.js
@@ -9,23 +9,15 @@ export default (state = initialState, action) => {
   switch (action.type) {
 
     case Constants.GET_SESSION_STATUS_DONE: {
-      let {
+      const {
         pdf_download_url:pdfDownloadUrl,
         document_name:documentName,
       } = action.payload;
 
-      if (!pdfDownloadUrl) {
-        pdfDownloadUrl = state.pdfDownloadUrl;
-      }
-
-      if (!documentName) {
-        documentName = state.documentName;
-      }
-
       return {
         ...state,
-        pdfDownloadUrl,
-        documentName,
+        ...(pdfDownloadUrl ? { pdfDownloadUrl } : {}),
+        ...(documentName ? { documentName } : {}),
       };
     }
 
