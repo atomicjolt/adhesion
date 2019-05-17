65e5ac572e8a6bcbe06009902a36cb96efaf505d
James Carbine <jamescarbine@gmail.com>
2019-05-16 18:42:52 -0600

    Delete old documents After we have had the document for more than a week, it is time to delete it. Delete all sessions related to the document as well.

--- app/models/atomic_doc.rb
+++ app/models/atomic_doc.rb
@@ -1,7 +1,9 @@
 class AtomicDoc < ApplicationRecord
-  has_many :atomic_doc_sessions
+  has_many :atomic_doc_sessions, dependent: :destroy
 
   before_save :ensure_status
+
+  scope :old, -> { where(arel_table[:created_at].lt(8.days.ago)) }
 
   private
 
