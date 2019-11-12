4fe6521a7ffcd381c57e0ef5a8a352e2cc68848a
James Carbine <jamescarbine@gmail.com>
2019-11-12 15:37:05 -0700

    Fix pie_chart to work with new recharts version

--- client/apps/scorm/components/analytics/pie_chart.jsx
+++ client/apps/scorm/components/analytics/pie_chart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import {
+  PieChart, Pie, Cell, Tooltip, Legend
+} from 'recharts';
 
 const Chart = (props) => {
   const pieFilling = props.data.map((key, index) => (
@@ -8,7 +10,14 @@ const Chart = (props) => {
   );
   return (
     <PieChart width={400} height={300}>
-      <Pie data={props.data} startAngle={90} endAngle={450} label labelLine>
+      <Pie
+        data={props.data}
+        dataKey="value"
+        startAngle={90}
+        endAngle={450}
+        label
+        labelLine
+      >
         {pieFilling}
       </Pie>
       <Legend layout="vertical" verticalAlign="top" align="right" />
