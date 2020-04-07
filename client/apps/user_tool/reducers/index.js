f9365512b357180caf319138dcde2cf5aaffb94a
Conrad Beach <conradbeach@protonmail.com>
2020-04-07 11:40:57 -0600

    feature: Remove error action and reducer
    They will be replaced with the `errors` reducers from the `atomic-fuel` package.

--- client/apps/user_tool/reducers/index.js
+++ client/apps/user_tool/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import settings from 'atomic-fuel/libs/reducers/settings';
 import jwt from 'atomic-fuel/libs/reducers/jwt';
 import application from './application';
-import error from './error';
 
 const rootReducer = combineReducers({
   settings,
   jwt,
   application,
-  error,
 });
 
 export default rootReducer;
