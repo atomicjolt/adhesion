0d856c9a209da42bcb6e47c94b874d969b556464
James Carbine <jamescarbine@gmail.com>
2020-09-14 17:52:21 -0600

    Update User model with devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :authentications, dependent: :destroy, inverse_of: :user
